b7353a17662e953d0f3a98ff88cc8119f4fe0435
Nev Simms <nev@upfrontmedia.asia>
2015-11-03 18:14:30 +0800

    finished arrays2

---
+++ arrays2-shell.php
@@ -0,0 +1,59 @@
+<?php
+$months = array('jan', 'feb', 'march' ,'apr', 'may');
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Arrays Pt 2</title>
+</head>
+<body>
+    <p>Add item to end of array</p>
+    <div>One way</div>
+    <?php
+    //two ways to add values to the array
+
+    
+    ?>
+    <div>Second way</div>
+    <?php
+    //short hand way
+
+    
+    ?>
+    <hr />
+    <p>remove item from end of array</p>
+    <?php
+
+    
+    ?>
+    <p>Keep a reference to the item removed</p>
+    <?php 
+
+    ?>
+    <div><strong>Note:</strong> This works with all of the array functions i.e. you can assign the result to a variable for subsequent use</div>
+    <hr />
+    <p>Remove item from beginning of array</p>
+    <?php 
+    
+
+    
+    ?>
+    <hr />
+    <p>add item to beginning af array</p>
+    <?php
+
+    ?>
+    <hr />
+    <p>Slice values to a new array get another array with feb and march</p>
+    <?php
+
+    ?>
+    <hr />
+    <p>Filter the array</p>
+    <?php
+
+    ?>
+
+</body>
+</html>
